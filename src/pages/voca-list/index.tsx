4daed0e6605a6fa27a392627a2bfa9f2a368d10b
sejung1218 <sejung1218@hanmail.net>
2023-04-04 16:11:20 +0900

    [Feat] 진짜 끝이야!

--- src/pages/voca-list/index.tsx
+++ src/pages/voca-list/index.tsx
@@ -35,17 +35,58 @@ export default function Page02() {
         </LearningListHeader>
         <Page02Container>
 
+
+
+          {/*<ListSection onClick={() => setShowModal(true)}>*/}
+          {/*  <ListStatusBox>*/}
+          {/*    <Typography sx={{fontSize: '15px', lineHeight: '25px', ml: '5px'}}>학습완료</Typography>*/}
+          {/*  </ListStatusBox>*/}
+          {/*  <ListTitleBox>*/}
+          {/*    <Typography sx={{fontSize: '25px', lineHeight: '40px', ml: '5px', width: '91%'}}>Part 1</Typography>*/}
+          {/*    <FiChevronRight style={{marginTop: '6px', fontSize: '1.7rem', fontWeight: '800', justifyContent:'flex-end'}}/>*/}
+          {/*  </ListTitleBox>*/}
+          {/*  <ListProgressWrap sx={{mt: '-5px'}}>*/}
+          {/*    <ListProgressBox>*/}
+          {/*      <ListProgressSuccessBar/>*/}
+          {/*    </ListProgressBox>*/}
+          {/*    <ListProgressString>*/}
+          {/*      <Typography sx={{fontSize: '11px', lineHeight: '20px', ml: '5px'}}>학습률: 121 / 121</Typography>*/}
+          {/*    </ListProgressString>*/}
+          {/*  </ListProgressWrap>*/}
+          {/*</ListSection>*/}
+
+          {/*{[...Array(0)].map((value, index) => (*/}
+          {/*  <ListSection key={index}>*/}
+          {/*    <ListStatusBox>*/}
+          {/*      <Typography sx={{fontSize: '15px', lineHeight: '25px', ml: '5px'}}>미학습</Typography>*/}
+          {/*    </ListStatusBox>*/}
+          {/*    <ListTitleBox>*/}
+          {/*      <Typography sx={{fontSize: '25px', lineHeight: '40px', ml: '5px', width: '91%'}}>Part {index + 1 + 1}</Typography>*/}
+          {/*      <FiChevronRight style={{marginTop: '6px', fontSize: '1.7rem', fontWeight: '800', justifyContent:'flex-end'}}/>*/}
+          {/*    </ListTitleBox>*/}
+          {/*    <ListProgressWrap sx={{mt: '-5px'}}>*/}
+          {/*      <ListProgressBox>*/}
+          {/*        <ListProgressBar/>*/}
+          {/*      </ListProgressBox>*/}
+          {/*      <ListProgressString>*/}
+          {/*        <Typography sx={{fontSize: '11px', lineHeight: '20px', ml: '5px'}}>학습률: 0 / 121</Typography>*/}
+          {/*      </ListProgressString>*/}
+          {/*    </ListProgressWrap>*/}
+          {/*  </ListSection>*/}
+          {/*))}*/}
+
           <ListSection onClick={() => setShowModal(true)}>
             <ListStatusBox>
-              <Typography sx={{fontSize: '15px', lineHeight: '25px', ml: '5px'}}>학습완료</Typography>
+              {/*<Typography sx={{fontSize: '15px', lineHeight: '25px', ml: '5px'}}>학습완료</Typography>*/}
+              {/*<img className={'imgimgimg'} src={'https://cdn.discordapp.com/attachments/1092315426643529748/1092693124549193758/1000_F_85589730_Lc3DaDkOfFtkmmXnewggPkWd3ZMKoXUz.png'}/>*/}
+              <img className={'imgimgimg'} src={'https://cdn.discordapp.com/attachments/1092315426643529748/1092704626685517824/1000_F_574288383_MIsTAjC100kIGIJxPbWfqvcSxzqhLgaS.png'}/>
             </ListStatusBox>
             <ListTitleBox>
-              <Typography sx={{fontSize: '25px', lineHeight: '40px', ml: '5px', width: '91%'}}>Part 1</Typography>
-              <FiChevronRight style={{marginTop: '6px', fontSize: '1.7rem', fontWeight: '800', justifyContent:'flex-end'}}/>
+              <Typography className={'ListTitleBoxTypo'} sx={{fontSize: '25px'}}>Part 1</Typography>
+              {/*<FiChevronRight className={'BackIcon'} style={{fontSize: '1.7rem', fontWeight: '800'}}/>*/}
             </ListTitleBox>
             <ListProgressWrap sx={{mt: '-5px'}}>
               <ListProgressBox>
-                <ListProgressSuccessBar/>
               </ListProgressBox>
               <ListProgressString>
                 <Typography sx={{fontSize: '11px', lineHeight: '20px', ml: '5px'}}>학습률: 121 / 121</Typography>
@@ -53,25 +94,6 @@ export default function Page02() {
             </ListProgressWrap>
           </ListSection>
 
-          {[...Array(0)].map((value, index) => (
-            <ListSection key={index}>
-              <ListStatusBox>
-                <Typography sx={{fontSize: '15px', lineHeight: '25px', ml: '5px'}}>미학습</Typography>
-              </ListStatusBox>
-              <ListTitleBox>
-                <Typography sx={{fontSize: '25px', lineHeight: '40px', ml: '5px', width: '91%'}}>Part {index + 1 + 1}</Typography>
-                <FiChevronRight style={{marginTop: '6px', fontSize: '1.7rem', fontWeight: '800', justifyContent:'flex-end'}}/>
-              </ListTitleBox>
-              <ListProgressWrap sx={{mt: '-5px'}}>
-                <ListProgressBox>
-                  <ListProgressBar/>
-                </ListProgressBox>
-                <ListProgressString>
-                  <Typography sx={{fontSize: '11px', lineHeight: '20px', ml: '5px'}}>학습률: 0 / 121</Typography>
-                </ListProgressString>
-              </ListProgressWrap>
-            </ListSection>
-          ))}
         </Page02Container>
       </EmergencyBox>
 
@@ -128,37 +150,62 @@ const LearningListHeader = styled.header`
 `
 
 const ListSection = styled(Box)`
-  width: 90%;
-  height: 100px;
+  width: 83%;
+  height: 350px;
   box-sizing: border-box;
   //border: 1px solid #d6d6d6;
   margin-bottom: 20px;
   border-radius: 10px;
   box-shadow: 5px 5px 5px 5px #d6d6d6;
-  padding: 3px;
+  //padding: 3px;
   cursor: pointer;
 `
 const ListStatusBox = styled(Box)`
   width: 100%;
-  height: 25%;
+  height: 40%;
   color: hotpink;
+  //object-fit: contain;
+  border: 1px solid red;
+  
+  .imgimgimg {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 10px 10px 0 0;
+  }
 `
 const ListTitleBox = styled(Box)`
   width: 100%;
-  height: 50%;
+  height: 15%;
   display: flex;
   flex-direction: row;
+  align-items: center;
+  vertical-align: center;
+  
+  .ListTitleBoxTypo {
+    margin-right: auto;
+    margin-left: 10px;
+  }
+  
+  .BackIcon {
+    color: blue;
+    margin-left: auto;
+    margin-right: 5px;
+  }
 `
 
 const ListProgressWrap = styled(Box)`
   width: 100%;
   height: 25%;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 15px;
 `
 
 const ListProgressBox = styled(Box)`
-  width: 70%;
+  width: 95%;
   height: 100%;
   display: flex;
   justify-content: center;
@@ -166,7 +213,10 @@ const ListProgressBox = styled(Box)`
 `
 
 const ListProgressSuccessBar = styled(Box)`
-  width: 98%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
   height: 90%;
   box-sizing: border-box;
   border: 2px solid pink;
@@ -183,10 +233,11 @@ const ListProgressBar = styled(Box)`
 `
 
 const ListProgressString = styled(Box)`
-  width: 30%;
   height: 100%;
+  margin-left: auto;
+  margin-right: 20px;
 `
-
+////////////////////////////////////////////////////////////////////////////////////
 const Modalchang = styled(Box)`
   position: fixed;
   top: 0;
@@ -204,7 +255,6 @@ const Modalchang = styled(Box)`
     border-radius: 10px;
     background-color: white;
     padding: 20px;
-    
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -219,7 +269,6 @@ const HiButton = styled(Button)`
     margin-top: 30px;
     margin-bottom: 5px;
   }
-
   &:last-of-type {
     margin-top: 30px;
   }
